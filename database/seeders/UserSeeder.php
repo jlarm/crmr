65a796adff5c03b8ea30979c55468bba0b286ed7
jlarm <117611704+jlarm@users.noreply.github.com>
2025-11-05 14:27:37 -0600

    Linting

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,13 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class UserSeeder extends Seeder
+final class UserSeeder extends Seeder
 {
-    public function run()
+    public function run(): void
     {
         User::factory(15)->create();
     }
